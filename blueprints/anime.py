3b86df11973df4c37ea178cb4e5cf5f0a1b881f8
Ayanokoji Kiyotaka <ayanokojix2306@gmail.com>
2025-11-13 08:20:24 +0100

    Fixed no return statements

--- blueprints/anime.py
+++ blueprints/anime.py
@@ -90,7 +90,7 @@ def anime_download():
             "message": "No results found"
         }), 404
     ep_count = info["episodes"]
-    if int(episode) > int(ep_count) or int(episode) == 0:
+    if int(episode) > int(ep_count):
         return jsonify({
             "status": 422,
             "message": "Episode number exceed available count"
@@ -100,8 +100,37 @@ def anime_download():
             "status": 404,
             "message": "No external id found"
         }), 404
-
-
+    if int(episode)<= 0:
+        return jsonify({
+            "status":400,
+            "message": "Episode count cannot be zero or below"
+        }),400
+    db = get_db()
+    cursor = db.execute(
+        "SELECT * FROM cached_video_url WHERE internal_id = ? and episode = ?", (id, episode))
+    row = cursor.fetchone()
+    if row and row["video_url"]:
+        link = row["video_url"]
+        res = requests.head(link, timeout=10)
+        if res.status_code == 200:
+            return jsonify({
+                "status": 200,
+                "direct_link": row["video_url"],
+                "size": row["size"],
+                "episode": row["episode"]
+            })
+    search_result = get_episode_session(info["external_id"])
+    episode_info = search_result[int(episode)-1]
+    episode_session = episode_info.get("session")
+    pahe_link = get_pahewin_link(info["external_id"], episode_session)
+    if pahe_link is None:
+        return jsonify({
+            "status": 404,
+            "message": "Internal Link not found"
+        }), 404
+    kiwi_url = asyncio.run(get_kiwi_url(pahe_link))
+    results = get_redirect_link(kiwi_url, id, episode)
+    return jsonify(results),500 if results.get("status") == 500 else 200
 @anime_bp.route("/bulk-download")
 def anime_bulk_downloading():
     id = request.args.get("id")
@@ -148,7 +177,7 @@ def anime_bulk_downloading():
         row = cursor.fetchone()
         if row and row["video_url"]:
             link = row["video_url"]
-            res = requests.head(link, timeout=5)
+            res = requests.head(link, timeout=10)
             if res.status_code == 200:
                 download_link.append({
                     "status": 200,
