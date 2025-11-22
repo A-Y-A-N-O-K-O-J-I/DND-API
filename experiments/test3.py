1471607e787f09fa5ceb9e7d708dc163a6e4f20d
Ayanokoji Kiyotaka <ayanokojix2306@gmail.com>
2025-11-22 18:20:14 +0100

    Fixed for huggingface?

--- experiments/test3.py
+++ experiments/test3.py
@@ -1,19 +1,4 @@
 import requests
 
-base_url = "https://access-kwik.apex-cloud.workers.dev"
-kwik_url = "https://kwik.cx/f/VFjuoZpRvYtH"
-payload = {
-        "service": "kwik",
-        "action": "fetch",
-        "content": {
-            "kwik": kwik_url
-        },
-        "auth": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.O0FKaqhJjEZgCAVfZoLz6Pjd7Gs9Kv6qi0P8RyATjaE"
-    }
-headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    }
-response = requests.post(base_url,data=payload,headers=headers,timeout=6)
-print(response.status_code)
-print(response.json())
+response = requests.get("https://animepahe.si")
+print(response.cookies)
--- helpers/anime_helper_kwik.py
+++ helpers/anime_helper_kwik.py
@@ -283,27 +283,19 @@ def get_redirect_link(url,id,episode):
     form_info = soup.find("form")
     size = soup.find("form").find(
         "span").get_text().split("(")[1].split(")")[0]
-    base_url = "https://access-kwik.apex-cloud.workers.dev"
-    payload = {
-        "service": "kwik",
-        "action": "fetch",
-        "content": {
-            "kwik": url
-        },
-        "auth": ""#"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.O0FKaqhJjEZgCAVfZoLz6Pjd7Gs9Kv6qi0P8RyATjaE"
+    base_url = "https://kwik-test.vercel.app/kwik"
+    params= {
+        "kwik_url":url
     }
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    }
-    res = requests.post(base_url,data=json.dumps(payload),headers=headers)
+    res = requests.get(base_url,params=params)
     if res.status_code != 200:
+        print(res.text)
         return {
             "status":500,
-            "message":"Server timed out, retry request"
+            "message":"Server timed out"
         }
     data = res.json()
-    direct_link = data.get("content").get("url")
+    direct_link = data.get("url")
     db.execute("INSERT OR REPLACE INTO cached_video_url(internal_id,episode,video_url,size) VALUES(?,?,?,?)",
                (id, episode, direct_link, size))
     db.commit()
