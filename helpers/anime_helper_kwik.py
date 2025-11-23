874326062bef25e826d7520c6c414034d0aefb81
Ayanokoji Kiyotaka <ayanokojix2306@gmail.com>
2025-11-23 18:34:44 +0100

    fixed

--- helpers/anime_helper_kwik.py
+++ helpers/anime_helper_kwik.py
@@ -202,19 +202,20 @@ def get_kiwi_info(kiwi_url):
         if not kiwi_url:
             return None
         headers = {
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-                'Accept-Language': 'en-US,en;q=0.9',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'DNT': '1',
-                'Connection': 'keep-alive',
-                'Upgrade-Insecure-Requests': '1',
-                'Sec-Fetch-Dest': 'document',
-                'Sec-Fetch-Mode': 'navigate',
-                'Sec-Fetch-Site': 'none',
-                'Sec-Fetch-User': '?1',
-                'Cache-Control': 'max-age=0',
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/131 Safari/537.36",
-            }
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate',  # remove 'br'
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Cache-Control': 'max-age=0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/131 Safari/537.36',
+}
+
         res = requests.get(kiwi_url,timeout=10,headers=headers)
         html_soup = BeautifulSoup(res.text,"html.parser")
         scripts = html_soup.find_all("script")
@@ -226,6 +227,7 @@ def get_kiwi_info(kiwi_url):
 
         }
     except IndexError:
+        print(res.text)
         print("Script is out of range -2")
         return None
     except Exception as e:
